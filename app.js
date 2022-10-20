0d0a040d86af7b66201009be2b743f9f0859ed2f
someonempty <someonempty@gmail.com>
2022-10-20 22:15:21 +0300

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const cols = document.querySelectorAll('.col');
+
+function generateRandomColor() {
+    const hexCodes = '0123456789ABCDEF';
+    let color = '';
+
+    for(let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color;
+}
+
+function setRandomColors() {
+    cols.forEach(col => {
+        
+    })
+}
